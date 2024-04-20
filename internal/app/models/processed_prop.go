e21a6ec64c4d006fed6e89bffca1fd805c4a2a0a
Abdirahman04 <abdixcom@gmail.com>
2024-04-20 08:01:44 +0300

    updated Addr method of ProcessedProp struct

--- internal/app/models/processed_prop.go
+++ internal/app/models/processed_prop.go
@@ -7,5 +7,17 @@ type ProcessedProp struct {
 }
 
 func (p ProcessedProp) Addr() (position int, classes []int) {
+  point := 100
+
+  if p.Id > point {
+    num := p.Id
+    for num > point {
+      classes = append(classes, num % point)
+      num = int(num / point)
+    }
+    position = num
+  } else {
+    position = p.Id
+  }
   return
 }
