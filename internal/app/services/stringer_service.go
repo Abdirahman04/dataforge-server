37e241a178ea379cb3fb236ce3e98e529b5c72c2
Abdirahman04 <abdixcom@gmail.com>
2024-04-21 12:42:22 +0300

    added StringerForge service function

---
+++ internal/app/services/stringer_service.go
@@ -0,0 +1,7 @@
+package services
+
+import "github.com/Abdirahman04/dataforge-server/internal/app/models"
+
+func StringerForge(prop models.Prop) models.ProcessedProp {
+  return models.ProcessedProp{}
+}
