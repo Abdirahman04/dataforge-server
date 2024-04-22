d98b106072d2ca03ef3ae916163d7525e55c6596
Abdirahman04 <abdixcom@gmail.com>
2024-04-22 20:12:14 +0300

    updated StringerForge

--- internal/app/services/stringer_service.go
+++ internal/app/services/stringer_service.go
@@ -11,6 +11,8 @@ func StringerForge(prop models.Prop) models.ProcessedProp {
     str = stringer.GetLetters(prop.Length)
   } else if prop.Class == "word" {
     str = stringer.GetSyllables(prop.Length)
+  } else if prop.Class == "email" {
+    str = stringer.GetEmail(prop.Length)
   }
 
   newProp := models.NewProcessedProp(prop.Id, prop.Name, str)
