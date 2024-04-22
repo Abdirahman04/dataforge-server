724d6ce86744270657139f0fd493c3fdbd24122e
Abdirahman04 <abdixcom@gmail.com>
2024-04-22 19:13:51 +0300

    added GetConsonants and GetVowels functions in stringer

--- pkg/stringer/main_stringer.go
+++ pkg/stringer/main_stringer.go
@@ -13,3 +13,27 @@ func GetLetters(ln int) string {
 
   return string(str)
 }
+
+func GetConsonants(ln int) string {
+  letters := [21]byte{'b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z'}
+
+  var str []byte
+
+  for i := 0;i < ln;i++ {
+    str = append(str, letters[rand.Intn(26)])
+  }
+
+  return string(str)
+}
+
+func GetVowels(ln int) string {
+  letters := [5]byte{'a','e','i','o','u'}
+
+  var str []byte
+
+  for i := 0;i < ln;i++ {
+    str = append(str, letters[rand.Intn(26)])
+  }
+
+  return string(str)
+}
