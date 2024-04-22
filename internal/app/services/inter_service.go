4deb5c7c97055caefd1556771736e61125cbadbe
Abdirahman04 <abdixcom@gmail.com>
2024-04-22 21:12:05 +0300

    updated InterForge

--- internal/app/services/inter_service.go
+++ internal/app/services/inter_service.go
@@ -1,22 +1,15 @@
 package services
 
 import (
-	"math"
-	"math/rand"
-
 	"github.com/Abdirahman04/dataforge-server/internal/app/models"
+	"github.com/Abdirahman04/dataforge-server/pkg/inter"
 )
 
 func InterForge(prop models.Prop) models.ProcessedProp {
   var num int
   if prop.Class == "random" {
-    num = randomIntLength(prop.Length)
+    num = inter.RandomNumber(prop.Length)
   }
 
   return models.NewProcessedProp(prop.Id, prop.Name, num)
-}
-
-func randomIntLength(ln int) int {
-  max := math.Pow(10, float64(ln))
-  return rand.Intn(int(max))
 }
