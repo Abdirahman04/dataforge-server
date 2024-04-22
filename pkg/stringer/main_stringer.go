2c77ffc18d320b73c1150b1b32647355663f1cff
Abdirahman04 <abdixcom@gmail.com>
2024-04-22 19:30:13 +0300

    added GetSyllables function in stringer

--- pkg/stringer/main_stringer.go
+++ pkg/stringer/main_stringer.go
@@ -37,3 +37,19 @@ func GetVowels(ln int) string {
 
   return string(str)
 }
+
+func GetSyllables(ln int) string {
+  cc1 := [5]byte{'c','g','s','t','n'}
+  cc2 := [2]byte{'h','l'}
+  ec := [10]byte{'b','d','g','k','l','m','n','p','s','t'}
+
+  if ln == 1 {
+    return GetLetters(1)
+  } else if ln == 2 {
+    return ec[rand.Intn(10)] + GetVowels(1)
+  } else if ln == 3 {
+    return cc1[rand.Intn(5)] + cc2[rand.Intn(2)] + GetVowels(1)
+  } else {
+    return GetLetters(ln)
+  }
+}
