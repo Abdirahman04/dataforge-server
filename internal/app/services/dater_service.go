8401ec02a676410de9a27b70660a78feb17a582c
Abdirahman04 <abdixcom@gmail.com>
2024-04-25 14:10:32 +0300

    updated DaterForge

--- internal/app/services/dater_service.go
+++ internal/app/services/dater_service.go
@@ -1,10 +1,17 @@
 package services
 
 import (
+	"time"
+
 	"github.com/Abdirahman04/dataforge-server/internal/app/models"
 	"github.com/Abdirahman04/dataforge-server/pkg/dater"
 )
 
 func DaterForge(prop models.Prop) models.ProcessedProp {
-  return models.NewProcessedProp(prop.Id, prop.Name, dater.CurrentDate())
+  var date time.Time
+
+  if prop.Class == "now" {
+    date = dater.CurrentDate()
+  }
+  return models.NewProcessedProp(prop.Id, prop.Name, date)
 }
