56fd564e3459c6f5bc052ea523a02b964b8ed174
Abdirahman04 <abdixcom@gmail.com>
2024-04-27 15:53:48 +0300

    updated ProcessedProp struct

--- internal/app/models/processed_prop.go
+++ internal/app/models/processed_prop.go
@@ -3,34 +3,15 @@ package models
 import "strconv"
 
 type ProcessedProp struct {
-  Id string
+  Id Id
   Name string
   Value interface{}
 }
 
-func NewProcessedProp(id, name string, val interface{}) ProcessedProp {
+func NewProcessedProp(id Id, name string, val interface{}) ProcessedProp {
   return ProcessedProp{
     Id: id,
     Name: name,
     Value: val,
   }
-}
-
-func (p ProcessedProp) Addr() (position int, classes []int) {
-  point, ln := 100, 2
-
-  position, _ = strconv.Atoi(p.Id[:2])
-  if len(p.Id) < ln {
-    return
-  }
-  cls, _ := strconv.Atoi(p.Id[2:])
-
-  if cls > point {
-    num := cls
-    for num > point {
-      classes = append(classes, num % point)
-      num = int(num / point)
-    }
-  }
-  return
 }
