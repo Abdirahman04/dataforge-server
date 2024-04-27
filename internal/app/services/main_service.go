e0622f0216658c13f7e8e5eb21f30165bbf73d46
Abdirahman04 <abdixcom@gmail.com>
2024-04-27 16:33:32 +0300

    updated CreateList function

--- internal/app/services/main_service.go
+++ internal/app/services/main_service.go
@@ -7,6 +7,7 @@ import (
 	"github.com/Abdirahman04/dataforge-server/internal/app/models"
 	"github.com/Abdirahman04/dataforge-server/pkg/dater"
 	"github.com/Abdirahman04/dataforge-server/pkg/inter"
+	"github.com/chenzhuoyu/iasm/obj"
 )
 
 func ListForge(b models.Blueprint) []map[string]interface{} {
@@ -15,34 +16,30 @@ func ListForge(b models.Blueprint) []map[string]interface{} {
   return objs
 }
 
-func CreateList(b models.Blueprint) []map[string]interface{} {
-  var objs []map[string]interface{}
+func CreateList(props []models.Prop) map[string]interface{} {
+  newObj := make(map[string]interface{})
 
-  for i := 0;i < b.Volume;i++ {
-    obj := make(map[string]interface{})
-    for _, e := range b.Props {
-      var prop models.ProcessedProp
-      if len(e.In) != 0 {
-        prop = GetFromIn(e)
-      } else if e.Range.Selected {
-        prop = GetFromRange(e)
-      } else if e.Type == "string" {
-        prop = StringerForge(e)
-      } else if e.Type == "int" {
-        prop = InterForge(e)
-      } else if e.Type == "float" {
-        prop = FloaterForge(e)
-      } else if e.Type == "bool" {
-        prop = BoolerForge(e)
-      } else if e.Type == "date" {
-        prop = DaterForge(e)
-      }
-      obj[e.Name] = prop.Value
+  for _, e := range props {
+    var prop models.ProcessedProp
+    if len(e.In) != 0 {
+      prop = GetFromIn(e)
+    } else if e.Range.Selected {
+      prop = GetFromRange(e)
+    } else if e.Type == "string" {
+      prop = StringerForge(e)
+    } else if e.Type == "int" {
+      prop = InterForge(e)
+    } else if e.Type == "float" {
+      prop = FloaterForge(e)
+    } else if e.Type == "bool" {
+      prop = BoolerForge(e)
+    } else if e.Type == "date" {
+      prop = DaterForge(e)
     }
-    objs = append(objs, obj)
+    newObj[prop.Name] = prop.Value
   }
 
-  return objs
+  return newObj
 }
 
 func GetFromIn(prop models.Prop) models.ProcessedProp {
